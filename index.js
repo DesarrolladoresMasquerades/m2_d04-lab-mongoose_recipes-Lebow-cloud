54d560327cff26f3ef9752b2c1d53c472c144a89
Daniel Milan <danielmilanvera@gmail.com>
2022-02-10 17:37:08 +0100

    Finish Lab

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
-const mongoose = require('mongoose');
+const dotenv = require("dotenv/config");
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI =
+  "mongodb+srv://Lebow:km6nevp2@cluster0.ezbcm.mongodb.net/recipe-app?authSource=admin&replicaSet=atlas-uwz0p3-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+
+    return Recipe.deleteMany();
   })
+
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    //ITERATION 3
+    Recipe.create(data);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+
+  .then(() => {
+    Recipe.find();
+  })
+  .then((recipe) => recipe.forEach((recipe) => console.log(recipe.title)))
+
+  .catch((error) => console.log(error))
+
+  //ITERATION 4
+
+  .then(() => {
+    Recipe.findOneAndUpdate(
+      { title: { $eq: "Rigatoni alla Genovese" } },
+      { $set: { duration: 100 } },
+      { new: true }
+    );
+  })
+
+  .then((recipe) => console.log(recipe))
+
+  .then(() => {
+    
+    //ITERATION 5
+    Recipe.deleteOne({ title: "Carrot Cake" }).then((recipe) =>
+      console.log(recipe)
+    );
+  })
+
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+  mongoose.connection.close()
